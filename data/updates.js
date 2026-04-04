58fd855859e9f9b15ecdc7dbd1e8323080c8e23e
CapnFiretail <cms12306@icloud.com>
2026-04-03 22:23:36 -0400

    Update updates.js

--- data/updates.js
+++ data/updates.js
@@ -20,6 +20,16 @@ const atlasUpdates = [
     ]
   },
   {
+    date: "April 3, 2026",
+    tag: "Update Log",
+    title: "Homepage Updates",
+    text: "Cleaned up structure and added new systems.",
+    list: [
+      "Added update log system",
+      "Built updates archive page",
+    ]
+  },
+  {
     date: "March 9, 2026",
     tag: "Eureka!",
     title: "The Pergamon Atlas is Born",
