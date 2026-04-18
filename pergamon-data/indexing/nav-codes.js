6b3fe5b513ec9a04e30a1a6db973a85daef6bd29
CapnFiretail <cms12306@icloud.com>
2026-04-17 23:54:00 -0400

    Update nav-codes.js

--- pergamon-data/indexing/nav-codes.js
+++ pergamon-data/indexing/nav-codes.js
@@ -1,5 +1,5 @@
 window.atlasNavCodes = {
-  "/":                      "SYSTEM-NAV-0-0-0-A100",
-  "/sectors/systems/tools": "SYSTEM-NAV-0-0-1-B100",
-  "/sectors/systems/games": "SYSTEM-NAV-0-0-2-C100"
+  "/":                      "SYSTEM-NAV-Z0-Y0-X0-A100",
+  "/sectors/systems/tools": "SYSTEM-NAV-Z0-Y0-X1-B100",
+  "/sectors/systems/games": "SYSTEM-NAV-Z0-Y0-X2-C100"
 };
